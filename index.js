4a376fe8421060d023a619008e8cb804b63000b7
ebtisamjelani <>
2023-04-09 10:46:47 +0000

    update readme

---
+++ index.js
@@ -0,0 +1,5 @@
+var firstName = "Ebtisam";
+var lastName = " Abdelkerim";
+var yearsOfStudy = " two years";
+var goal = "have a better understanding of Java Script language ";
+console.log("My name is " + firstName + lastName +". I have been at SCC for " + yearsOfStudy +" and plan to " + goal+ "after completing my courses.");
---
+++ script.js
@@ -0,0 +1,4 @@
+var firstName = "Ebtisam";
+var lastName = "Abdelkerim";
+var yearsOfStudy = "two years";
+var goal = "have a better understanding of Java Script language.";
