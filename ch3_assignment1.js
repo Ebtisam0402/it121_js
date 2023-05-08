c1c9a8e1edd87b7e890613375d48d4db34ee7b1c
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2023-05-07 23:05:46 -0700

    Delete ch3_assignment1.js

--- ch3_assignment1.js
+++
@@ -1,8 +0,0 @@
-const people = {friends:[]};
-const friend1 = {firstname: "Elham", lastname: "Saed", ID: "4226"};
-const friend2 = {firstname: "Zuhur", lastname: "Ahmed", ID: "5698"};
-const friend3 = {firstname: "Manar", lastname: "Said", ID: "9021"};
-people.friends.push(friend1,friend2,friend3);
-console.log(people.friends);
-
-// push is a connector for these three friends.
